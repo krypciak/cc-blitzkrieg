b82ce704ef61d58ff6f5d2277e5bd35354381acd
krypek <krypek@tuta.io>
2023-10-07 22:26:02 +0200

    Add cc-blitzkrieg-types

--- dist/tab.d.ts
+++
@@ -1,5 +0,0 @@
-export function setupTabs(): void;
-export function prepareTabFonts(): void;
-export function isBlitzkriegEnabled(): unknown;
-export function setBlitzkriegEnabled(bool: any): void;
-export function getBlitzkriegTabIndex(): any;
