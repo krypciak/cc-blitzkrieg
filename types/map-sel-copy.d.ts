821fb041c5f4c22d5e808bc6b146a58315c2686a
krypek <krypek@tuta.io>
2024-08-25 17:48:51 +0200

    Autogenerate ./types

--- types/map-sel-copy.d.ts
+++ types/map-sel-copy.d.ts
@@ -1,7 +1,9 @@
 import { Selection } from './selection';
 import { EntityRecArgs, MapCopyOptions } from 'cc-map-util/src/map-copy';
+import { PuzzleSelection } from './puzzle-selection';
+import { BattleSelection } from './battle-selection';
 export declare class BlitzkriegMapUtil {
-    copySelMapAreaTo(baseMap: sc.MapModel.Map, selMap: sc.MapModel.Map, sel: Selection, offset: Vec2, filters: EntityRecArgs['filters'], options: MapCopyOptions): sc.MapModel.Map;
+    copySelMapAreaTo(baseMap: sc.MapModel.Map, selMap: sc.MapModel.Map, sel: Selection, offset: Vec2, filters: EntityRecArgs['filters'], options: MapCopyOptions, copySels?: (PuzzleSelection | BattleSelection)[]): sc.MapModel.Map;
     copyCurrentSel(): Promise<void>;
     getMapStringByPath(path: string): Promise<string>;
     getMapObjectByPath(path: string): Promise<sc.MapModel.Map>;
